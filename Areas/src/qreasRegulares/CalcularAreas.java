112a12a2c417e02ff2a9fc9322084c9c9980d347
dario <dario@profesor-223>
2025-03-21 19:34:20 +0100

    Declarar variables base y altura, asignamos valores

--- Areas/src/qreasRegulares/CalcularAreas.java
+++ Areas/src/qreasRegulares/CalcularAreas.java
@@ -4,7 +4,10 @@ public class CalcularAreas {
 
 	public static void main(String[] args) {
 		//Programa para calcular áreas
-
+		//Declarar variables base y altura, asignamos valores
+		int base=5;
+		int altura=10;
+		
 	}
 
 }
